77d9027daa38fbd35e71ff121038143870f8d489
jw56578 <jw56578@gmail.com>
2015-11-17 19:09:23 -0600

    more stuffs

---
+++ CSharpEverything/CSharpEverything/Locking.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace CSharpEverything
+{
+    /// <summary>
+    /// https://msdn.microsoft.com/en-us/library/system.threading.monitor(v=vs.110).aspx
+    /// http://blog.coverity.com/2014/02/12/how-does-locking-work/#.VkqSd7erSUk
+    /// </summary>
+    [TestClass]
+    public class Locking
+    {
+        [TestMethod]
+        public void Unlocked()
+        {
+            Thread obj = new Thread(DivideUnlocked);
+            obj.Start();
+            Thread.Sleep(1000);
+            DivideUnlocked();
+        }
+        [TestMethod]
+        public void MonitorLocked()
+        {
+            Thread obj = new Thread(DivideUnlocked);
+            obj.Start();
+            Thread.Sleep(1000);
+            DivideMonitorLock();
+        }
+        [TestMethod]
+        public void LockLocked()
+        {
+            Thread obj = new Thread(DivideUnlocked);
+            obj.Start();
+            Thread.Sleep(1000);
+            DivideLockLock();
+        }
+        static int num1 = 0;
+        static int num2 = 0;
+        static Random random = new Random();
+        static readonly object _object = new object();
+        /// <summary>
+        /// this should break because one thread is setting num2 back to zero while another thread is still operating on it
+        /// currently doesn't break though, not sure why
+        /// </summary>
+        static void DivideUnlocked()
+        {
+            for (int i = 0; i <= 999999; i++)
+            {
+                try
+                {
+                    //Choosing random numbers between 1 to 5
+                    num1 = random.Next(1, 5);
+                    num2 = random.Next(1, 5);
+                    //Dividing
+                    double ans = num1 / num2;
+                    //Reset Variables
+                    num2 = 0;
+                    num1 = 0;
+                   
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+        /// <summary>
+        /// Monitor is a computer science concept for blocking threads access to things
+        /// https://en.wikipedia.org/wiki/Monitor_(synchronization)
+        /// all it does is make a thread not able to access any object, which then in turn makes it not be able to go further in the code
+        /// </summary>
+        static void DivideMonitorLock()
+        {
+            Monitor.Enter(_object);
+            try
+            {
+                for (int i = 0; i <= 999999; i++)
+                {
+                    try
+                    {
+                        //Choosing random numbers between 1 to 5
+                        num1 = random.Next(1, 5);
+                        num2 = random.Next(1, 5);
+                        //Dividing
+                        double ans = num1 / num2;
+                        //Reset Variables
+                        num2 = 0;
+                        num1 = 0;
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+            }
+            finally {
+                Monitor.Exit(_object);
+            }
+        }
+        /// <summary>
+        /// the lock syntax is just sugar over the Monitor Enter/exit in a try finally block
+        /// </summary>
+        static void DivideLockLock()
+        {
+            lock(_object)
+            {
+                for (int i = 0; i <= 999999; i++)
+                {
+                    try
+                    {
+                        //Choosing random numbers between 1 to 5
+                        num1 = random.Next(1, 5);
+                        num2 = random.Next(1, 5);
+                        //Dividing
+                        double ans = num1 / num2;
+                        //Reset Variables
+                        num2 = 0;
+                        num1 = 0;
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ CSharpEverything/CSharpEverything/Mutex.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace CSharpEverything
+{
+    /// <summary>
+    /// http://www.onlinebuff.com/article_understand-monitor-vs-mutex-vs-semaphore-vs-semaphoreslim-onlinebuff_60.html
+    /// an example of using a mutex is when you don't want 2 instances of an application running at the same time
+    /// normally you can just double click on an exe twice and the app would be running in 2 different processes
+    /// none of this can actually be tested because you can' run multiple instances of Unit tests
+    /// </summary>
+    [TestClass]
+    public class MutexTests
+    {
+        [TestMethod]
+        public void TestMutex()
+        {
+            using (Mutex m1 = new Mutex(true, "MyAppLabel"))
+            {
+                //this will make any thread wait for 5 seconds if the app is already running to see if it stops running
+                if (!m1.WaitOne(5000, false))
+                { 
+                    //this is what will happen when after 5 seconds another process is still running the app
+                }
+                
+
+            }
+
+        }
+        /// <summary>
+        /// the semephore allows you to specify that only a certain number of threads can access an area at one time
+        /// </summary>
+        [TestMethod]
+        public void TestSemephore()
+        {
+            using (Semaphore m1 = new Semaphore(3,3, "MyAppLabel"))
+            {
+                //this will make any thread wait for 5 seconds if the app is already running to see if it stops running
+                m1.WaitOne();
+                //if the thread is done it can release the lock
+                m1.Release();
+                
+
+
+            }
+
+        }
+    }
+}
