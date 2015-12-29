61527af369552bb7acac45f60b1346d991c996c9
jw56578 <jw56578@gmail.com>
2015-12-28 19:13:18 -0600

    remove static constructor to make it faster

--- CSharpEverything/CSharpEverything/SimpleGenericRepository.cs
+++ CSharpEverything/CSharpEverything/SimpleGenericRepository.cs
@@ -9,18 +9,46 @@ namespace CSharpEverything
     /// this is the simplist version of what i'm trying to accomplish with a dynamic generic repository thing
     /// using the trick of static constructors to cache the mapping of repositories to types
     /// you can't have an interace definition and then implmment with a derived type as the method parameter
+    /// this appears to be too slow, it is N2.5 times slower than just filling the collection directly
+    /// if I remove the static variable and just instantiate an new repository each time it is as fast
+    /// why is accessing a static thing so slow?
+    /// i put the static field back in and just assign it directly to the personfiller and its almost just as fast WTF
+    /// holy crap, using a static constructor was making it so slow
+    /// http://blogs.msdn.com/b/brada/archive/2004/04/17/115300.aspx
     /// </summary>
     [TestClass]
     public class SimpleGenericRepository
     {
+        int repeats = 5000000;
+
         [TestMethod]
         public void TestSimpleGenericRepository()
         {
-            var personFiller = new DataService<Person>();
-            List<Person> coll = new List<Person>();
-            PersonFiller f = new PersonFiller();
-            f.Fill(coll);
-            personFiller.Fill(coll);
+            while (true)
+            {
+                Concrete();
+                Generic();
+            }
+
+        }
+
+        void Concrete()
+        {
+            for (int i = 0; i < repeats; i++)
+            {
+                List<Person> coll = new List<Person>();
+                PersonFiller f = new PersonFiller();
+                f.Fill(coll);
+            }
+        }
+        void Generic()
+        {
+            for (int i = 0; i < repeats; i++)
+            {
+                var personFiller = new DataService<Person>();
+                List<Person> coll = new List<Person>();
+                personFiller.Fill(coll);
+            }
         }
     }
     public static class RepositoryMapper
@@ -38,18 +66,13 @@ namespace CSharpEverything
     }
     public class DataService<T> 
     {
-        static IRepository<T> f;
-        static DataService()
-        {
-            f = RepositoryMapper.GetMap(typeof(T).FullName) as IRepository<T>;
-        }
+        static IRepository<T> f = RepositoryMapper.GetMap(typeof(T).FullName) as IRepository<T>;
+
         public void Fill(List<T> collection)
         {
-            if (f == null)
-                throw new Exception("there is no Repository for this type");
             f.Fill(collection); 
         }
-        public List<T> Get()
+        public IEnumerable<T> Get()
         {
             var list = new List<T>();
             this.Fill(list);
