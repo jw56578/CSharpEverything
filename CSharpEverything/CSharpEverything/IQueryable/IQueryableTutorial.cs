fa2d380b9b710b69adf330e3de24845af864106e
Jon Woo <jw56578@gmail.com>
2016-09-18 19:01:23 -0500

    more stuff

---
+++ CSharpEverything/CSharpEverything/IQueryable/IQueryableTutorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpEverything
+{
+    [TestClass]
+    public class IQueryableTutorial
+    {
+        [TestMethod]
+        public void WhatDoesIQueryableDo()
+        {
+            List<int> numbers = new List<int> { 1, 2, 4, 5, 6, 7, 8 };
+            //this will not work because a List is not IQueryable
+            //if you look at the Where extension method, it only takes an IQueryable
+            //var result = from x in queryablenumbers
+            //             where x > 0
+            //             select x * x;
+
+            var queryablenumbers = numbers.AsQueryable();
+            var result = from x in queryablenumbers
+                         where x > 0
+                         select x * x;
+
+            result = queryablenumbers.Where(n => n > 0);
+
+           
+
+
+        }
+    }
+}
